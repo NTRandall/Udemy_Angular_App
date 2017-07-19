84d0a33518c866c6560ff5ca168e6090e3a422ec
Nathan Randall <nathan.randall@TCC.local>
2017-07-19 17:15:20 -0400

    server name 2 way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,15 +2,27 @@
 
 @Component({
   selector: 'app-servers',
-  template: `<app-server></app-server>
-             <app-server></app-server>`,
+  //template: `<app-server></app-server>
+  //           <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+    allowNewServer = false;
+    serverCreationStatus = 'No Server for you!';
+    serverName = 'Testserver';
 
-  constructor() { }
+    constructor() {
+        setTimeout(() => { this.allowNewServer = true; } , 2000)
+    }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+      this.serverCreationStatus = ' was created.';
+  }
+  onUpdateServerName(event: Event) {
+      this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
